691168a2661ef37919975f06e02c6815f76030fe
Mahesh-addagatla <maheshaddagatla1234@gmail.com>
2024-07-19 18:33:58 +0530

    topic Solved

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ProblemSolvedChart from "./components/problemSolvedChart/ProblemSolvedChart2";
 import TopicSolved from "./components/topicsolved/TopicSolved";
 import DonutChart from "./components/donutchart/DonutChart";
