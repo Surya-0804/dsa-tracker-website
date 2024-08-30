80168bbb946b89cc5ba6d7cc7280385ac7a48f10
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-08-30 12:12:22 +0530

    gfg fetch try

--- src/pages/profile/components/geeksforgeeks/GeeksforGeeks.jsx
+++ src/pages/profile/components/geeksforgeeks/GeeksforGeeks.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 import GFG from "./GFG.svg";
 import { LuMedal } from "react-icons/lu";
 import DonutChart from "../donutchart/DonutChart";
 
 export default function GeeksforGeeks() {
+  useEffect(() => {
+    const fetchGfgStats = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_URL}/codingStats/gfgStats`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ userName: "venkatmiriyala" }),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch: ${response.status} ${response.statusText}`
+          );
+        }
+
+        const data = await response.json();
+        console.log("Fetched GFG stats data:", data);
+      } catch (error) {
+        console.error("Error fetching GFG stats:", error);
+      }
+    };
+
+    fetchGfgStats();
+  }, []);
+
   return (
     <div className="profile-page-geeksforgeeks-container">
       <h1 className="profile-page-geeksforgeeks-header">
-        GeeksforGeeks <img src={GFG} alt="GFG" />{" "}
+        GeeksforGeeks <img src={GFG} alt="GFG" />
       </h1>
 
       <div className="profile-page-geeksforgeeks-inner-container">
@@ -43,7 +75,7 @@ export default function GeeksforGeeks() {
             </div>
           </div>
         </div>
-        <div className="profile-page-geeksforgeeks-vertical-line"> </div>
+        <div className="profile-page-geeksforgeeks-vertical-line"></div>
         <div
           style={{ width: "50%" }}
           className="profile-page-geeksforgeeks-donutchart"
